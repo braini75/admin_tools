c49f03edfe641fa8fbac2153456302622e52762e
Thomas K <braini@sonnenkinder.org>
2016-10-24 16:23:26 +0200

    Added some Testing

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :admin_tools do
   name 'Admin Tools plugin'
   author 'Thomas Koch'
   description 'This is a plugin for Redmine poviding Admin Tools'
-  version '0.1'
+  version '0.2'
   url 'https://github.com/braini75/admin_tools'
   author_url 'https://github.com/braini75'
   
--- test/functional/admin_tools_controller_test.rb
+++ test/functional/admin_tools_controller_test.rb
@@ -1,8 +1,20 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class AdminToolsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  tests AdminToolsController
+  fixtures :users 
+  
+  setup do
+    session[:user_id] = 1
   end
+  
+  test 'should render show_config' do
+	get :show_config
+    assert_response :success	
+	
+    Setting.all.each do |setting|
+	  assert_select 'label', {:text=>"Name: #{setting.name}"}
+    end
+  end
+
 end
